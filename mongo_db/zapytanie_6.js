67a0c0168eb0c68671455b4720d422188cc55ac8
marcinkielek <73818647+marcinkielek@users.noreply.github.com>
2021-06-28 16:20:13 +0200

    Add files via upload

---
+++ mongo_db/zapytanie_6.js
@@ -0,0 +1,27 @@
+printjson(db.people.insertOne({
+"sex" : "Male",
+"first_name" : "Marcin",
+"last_name" : "Kielek",
+"job" : "",
+"email" : "kontodospamuigier@wp.pl",
+"location" : {
+"city" : "Sac Escobar City",
+"address" : {
+"streetname" : "Numertocztery",
+"streetnumber" : "4"
+}
+},
+"description" : "Average Life Enjoyer",
+"height" : "178",
+"weight" : "75",
+"birth_date" : "1998-06-25T09:15:01Z",
+"nationality" : "San Escobar",
+"credit" : [
+{
+"type" : "switch",
+"number" : "1029384756",
+"currency" : "Not_Crypto",
+"balance" : "12345"
+}
+]
+}))
